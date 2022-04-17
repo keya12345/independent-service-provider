21f5539e1a7b7b7c5e41cefd2d9ad14aee326fa1
keya12345 <0319452930@student.wub.edu.bd>
2022-04-17 22:36:09 +0600

    route add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Footer from "./Pages/Home/Footer/Footer";
 import Home from "./Pages/Home/Home/Home";
 import PageNotFound from "./Pages/About/PageNotFound/PageNotFound";
 import About from "./Pages/About/About/About";
-
+import Login from "./Pages/Log/Login/Login";
+import Register from "./Pages/Log/Register/Register";
+import Checkout from "./Pages/About/Checkout/Checkout";
+import RequireAuth from "./Pages/Log/ReqireAuth/ReqireAuth";
+import ServiceDetail from "./Pages/About/ServiceDetail/ServiceDetail";
 function App() {
   return (
     <div>
@@ -14,6 +18,21 @@ function App() {
         <Route path="/" element={<Home></Home>} />
         <Route path="/home" element={<Home></Home>} />
         <Route path="/about" element={<About></About>} />
+        <Route path="/login" element={<Login></Login>} />
+        <Route
+          path="/service/:serviceId"
+          element={<ServiceDetail></ServiceDetail>}
+        />
+
+        <Route path="/register" element={<Register></Register>} />
+        <Route
+          path="/about"
+          element={
+            <RequireAuth>
+              <Checkout></Checkout>
+            </RequireAuth>
+          }
+        />
 
         <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
       </Routes>
--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,47 +1,79 @@
 import React from "react";
+import { getAuth, signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 import "./Header.css";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../../../image/logo/logo-1.png";
+import app from "../../../firebase.init";
 const Header = () => {
-  return (
-    <>
-      <div className="nav-container">
-        <Navbar bg="dark" variant="dark" sticky="top">
-          <Container className="d-flex">
-            <div>
-              {" "}
-              <Navbar.Brand as={Link} to="/">
-                <img height={50} width={100} src={logo} alt="" />
-              </Navbar.Brand>
-            </div>
-            <div>
-              <Nav className="me-auto">
-                <button>Booking Now</button>
-              </Nav>
-            </div>
-          </Container>
-        </Navbar>
+  const auth = getAuth(app);
+  const [user] = useAuthState(auth);
 
-        <Navbar bg="primary" variant="dark" sticky="top">
-          <Container className="d-flex">
-            <div>
-              {" "}
-              <Navbar.Brand as={Link} to="/">
-                <h2> Photographer</h2>
-              </Navbar.Brand>
-            </div>
-            <div>
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-              </Nav>
-            </div>
-          </Container>
-        </Navbar>
-      </div>
-    </>
+  const handleSignOut = () => {
+    signOut(auth);
+  };
+
+  return (
+    <div className="nav-container">
+      <Navbar bg="dark" variant="dark" sticky="top">
+        <Container className="d-flex">
+          <div>
+            {" "}
+            <Navbar.Brand as={Link} to="/">
+              <img height={50} width={100} src={logo} alt="" />
+            </Navbar.Brand>
+          </div>
+          <div>
+            <Nav className="me-auto">
+              <button>Booking Now</button>
+            </Nav>
+          </div>
+        </Container>
+      </Navbar>
+
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        bg="primary"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <h2>PHOTOGRAPHER</h2>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="home#services" className="fs-5 fw-normal ">
+                Services
+              </Nav.Link>
+              <Nav.Link href="home#experts" className="fs-5 fw-normal ">
+                OutdoorPhotos
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={Link} to="about" className="fs-5 fw-normal ">
+                About
+              </Nav.Link>
+              {user ? (
+                <button
+                  className="btn btn-link text-white text-decoration-none fs-5 fw-normal"
+                  onClick={handleSignOut}
+                >
+                  sign out
+                </button>
+              ) : (
+                <Nav.Link as={Link} to="login" className="fs-5 fw-normal ">
+                  Login
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
